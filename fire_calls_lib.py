8d531c39abd6ef3e40cce024978dc87a2828ca96
jeff-dillon <dillonjg@gmail.com>
2022-09-09 16:25:49 -0400

    Refactor count_values_by_time_of_day

--- fire_calls_lib.py
+++ fire_calls_lib.py
@@ -14,6 +14,25 @@ def count_values(data : list, key : str) -> dict:
             result[record[key]] += 1
     return result
 
+def count_values_by_time_of_day(data : list, tod : str) -> dict:
+    """Convenience function to summarize calls by time of day and day of week.
+
+    :param: data - a list of dicts
+    :param: tod - the time of day to match for counting
+    "returns: a dictionary of days of week and number of calls
+    """
+    result = {}
+
+    for record in data:    
+        day_of_week = record['DAY_OF_WEEK']
+        time_of_day = record['TIME_OF_DAY']
+        if time_of_day == tod:
+            if day_of_week in result:
+                result[day_of_week] += 1
+            else:
+                result[day_of_week] = 1
+    return result
+
 def get_unique_values(data : list, key : str) -> list:
     """Convenience function to get the unique values for a key in a list of 
     dict objects
